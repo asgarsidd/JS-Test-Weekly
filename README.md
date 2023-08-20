bbe39a5c157ffc4b710485075eb4c50d72cd848f
asgarsidd <asgar2feb@gmail.com>
2023-08-20 21:06:03 +0530

    Task is completed

---
+++ README.md
@@ -0,0 +1,12 @@
+# JS-test-20
+
+This is a Drum kit project.
+in this project you can play sounds of drum when particular image is clicked.
+Technologies used are :
+html
+css
+javascript
+working :
+when you click on particular drum then a function is called and sound will play
+there are different functions for different sounds.
+Hosted Link - https://github.com/
---
+++ assets/index.js
@@ -0,0 +1,28 @@
+function w(){
+    let sound = new Audio("./assets/sounds/crash.mp3");
+    sound.play();
+}
+function a(){
+    let sound = new Audio("./assets/sounds/tom-1.mp3");
+    sound.play();
+}
+function s(){
+    let sound = new Audio("./assets/sounds/tom-3.mp3");
+    sound.play();
+}
+function d(){
+    let sound = new Audio("./assets/sounds/tom-4.mp3");
+    sound.play();
+}
+function j(){
+    let sound = new Audio("./assets/sounds/snare.mp3");
+    sound.play();
+}
+function k(){
+    let sound = new Audio("./assets/sounds/crash.mp3");
+    sound.play();
+}
+function l(){
+    let sound = new Audio("./assets/sounds/kick-bass.mp3");
+    sound.play();
+}
